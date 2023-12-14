45e33cb35554afa583a3fb1536be0db5093a5e57
dhirajkalwar57 <dhirajkalwar57@gmail.com>
2023-12-14 15:35:06 +0530

    cors update

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
     res.header('Access-Control-Expose-Headers', 'jwtoken');
+    res.setHeader('Access-Control-Allow-Origin', '*')
     next();
   });
 app.use(cookieParser());
